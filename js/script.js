7acd26b93fb516bcf1fe1cda81929c360ea61187
Tolebi <tolebizaksybaj@gmail.com>
2020-11-16 23:21:43 +0600

    js slides

--- js/script.js
+++ js/script.js
@@ -163,12 +163,41 @@ window.addEventListener('DOMContentLoaded', function () {
 	showSlides(sliderIndex);
 
 	function showSlides(n) {
-		slides.forEach((item) => item.style.display = 'none');
 
+		if (n > slides.length) {
+			sliderIndex = 1
+		}
+		if (n < 1) {
+			sliderIndex = slides.length;
+		}
+
+		slides.forEach((item) => item.style.display = 'none');
 		dots.forEach((item) => item.classList.remove('dot-active'));
 
 		slides[sliderIndex - 1].style.display = 'block';
 		dots[sliderIndex - 1].classList.add('dot-active');
 	}
+
+	function plusSlides(n) {
+		showSlides(sliderIndex += n);
+	}
+	function currentSlides(n) {
+		showSlides(sliderIndex = n);
+	}
+
+	prev.addEventListener('click', function () {
+		plusSlides(-1);
+	});
+	next.addEventListener('click', function () {
+		plusSlides(1);
+	});
+
+	dotsWrap.addEventListener('click', function (event) {
+		for (let i = 0; i < dots.length + 1; i++) {
+			if (event.target.classList.contains('dot') && event.target == dots[i - 1]) {
+				currentSlides(i);
+			}
+		}
+	});
 
 });
